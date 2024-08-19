36d0c8c6225b0947b704fa2196a3043f5ff26e65
QOALA <qoala@Q0396.local>
2024-08-19 17:24:06 +0530

    Adding support for slighlty disoriented image as well

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import json 
 import utils_math as um 
 from ocr import bounding_box
-from utils_image import rotate_image
+from utils_image import rotate_image, draw_box
 import time
 import cv2 
-
+from PIL import Image
 tstart = time.time()
 with open('config.json', 'r') as file: 
     config = json.load(file)
@@ -13,13 +13,22 @@ print("ocr_started")
 start = time.time()
 coordinate = bounding_box(config['image_path'], config['vision_api_key'])
 end = time.time()
+print(coordinate)
 print("ocr_done", end-start)
 
 
 start = time.time()
 car = coordinate['Car']
 tires = coordinate['Tires']
-
+img = cv2.imread(config['image_path'])
+img = draw_box(img, car)
+print(f'car coordeinates: {car}')
+for tire in tires: 
+    print(f'tire coordinates {tire}')
+    img = draw_box(img, tire)
+# cv2.imshow("bounding_boxes", img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 breadth = um.distince(car[0]['x'], car[1]['x'], car[0]['y'], car[1]['y'])
 length = um.distince(car[0]['x'], car[3]['x'], car[0]['y'], car[3]['y'])
 
@@ -27,47 +36,56 @@ centriod = {
                 'x':(car[1]['x'] + car[0]['x'])/2, 
                 'y':(car[3]['y'] + car[0]['y'])/2
             }
+print(f'centroid {centriod}')
 t1center = {
-    'x':(tires[0][1]['x'] + tires[0][0]['x'])/2, 
-    'y':(tires[0][3]['y'] + tires[0][0]['y'])/2
+    'x':(tires[0][3]['x'] + tires[0][2]['x'])/2, 
+    'y':(tires[0][3]['y'] + tires[0][2]['y'])/2
 }
+print(f't1 center is:{t1center}')
 t2center = {
-    'x':(tires[1][1]['x'] + tires[1][0]['x'])/2, 
-    'y':(tires[1][3]['y'] + tires[1][0]['y'])/2
+    'x':(tires[1][3]['x'] + tires[1][2]['x'])/2, 
+    'y':(tires[1][3]['y'] + tires[1][2]['y'])/2
 }
+print(f't2 center is:{t2center}')
 tcenter = {
     'x':(t1center['x'] + t2center['x'])/2, 
     'y':(t1center['y'] + t2center['y'])/2
 }
-
-if length > breadth:
-    rotaion_angle, comp_rotation_angle = um.get_angle(car[0], car[3])
-    print(f'rotation_angle {rotaion_angle}, count_rotation_angle {comp_rotation_angle}')
-
-    # rotated_image = rotate_image(config['image_path'], rotaion_angle)
-    # comp_rotated_image = rotate_image(config['image_path'], comp_rotation_angle)
-    end = time.time()
-    print("Rotation Angle Time",end-start)
-
+print(f'The avg center of tires = {tcenter}')
+if t1center['y'] != t2center['y']:
     start = time.time()
-    angle = [rotaion_angle, comp_rotation_angle]
-    for theta in angle:
-        print(f"for angle {theta}")
+    rotation_angle, comp_rotation_angle = um.get_angle(t1center, t2center)
+    end = time.time()
+    print("rotation angle time", end-start)
+    print(f'rotation_ange: {rotation_angle}, comp_rotation_angle: {comp_rotation_angle}')
+    angle = [rotation_angle, comp_rotation_angle]
+    for theta in angle: 
+        print(f'for angke {theta}')
         rcentroid = um.rotate_point(centriod, theta)
         print(f'original car centroid ={centriod}, rotated centroid={rcentroid}')
         rtcenter = um.rotate_point(tcenter, theta)
         print(f'original tire centroid ={tcenter}, rotated centroid={rtcenter}')
-        if rcentroid['y'] >= rtcenter['y']:
-            correct_rotation = theta
-    end = time.time()
-    print(f'correct_rotation: {correct_rotation}') 
-    print("time takes for correct rotation calculation", end - start)
-    tend = time.time()
-    print("total_time:", tend-tstart)
-    output_image = rotate_image(config['image_path'], rotaion_angle)
+        for i in range(4): 
+            print(f'no {i} car coord: {um.rotate_point(car[i], theta)}')
+            print(f'no {i} tire coord: {um.rotate_point(tire[i], theta)}')
+
+        if rcentroid['y'] < rtcenter['y']:
+            corect_rotation = theta
+    
+    print(f'Correct_rotation {corect_rotation}')
+    if corect_rotation > 0:         
+        output_image = rotate_image(config['image_path'], corect_rotation)
+    else: 
+        output_image = rotate_image(config['image_path'],360- corect_rotation)
+
+    # cv2.imshow("correct_rotated_image", output_image)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
 else: 
     config['image_path']
-
+tend = time.time()
+print("total time takes:", tend-tstart)
 # image = cv2.imread(config['image_path'])
 # img = draw_box(image, car)
 # for box in tires: 
